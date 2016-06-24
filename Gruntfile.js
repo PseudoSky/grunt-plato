503f2b2f582c9c426d355c9caf633dd37a80228b
snow <kkrautha@andrew.cmu.edu>
2016-06-23 21:01:57 -0700

    Added es6 plato

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,7 +91,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['jshint', 'clean', 'plato', 'nodeunit']);
+  grunt.registerTask('test', ['jshint', 'clean', 'plato']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
